a7681308ca5302f2b8a2dabceaf90c1a5828d2b1
kuon9812 <kimjunho7725@naver.com>
2025-08-12 16:45:58 +0900

    Day_12

---
+++ C_01/포인터상수.c
@@ -0,0 +1,33 @@
+/*
+const int *p -- > 값이 상수가 된다! ( 주소 변경이 가능 : 값 변경 불가)
+int const *p -- > 값이 상수가 된다! ( 주소 변경 가능 : 값 변경 불가 )
+int*const p -- > 포인터(주소) 상수 ( 주소 변경 불가 : 값 변경 가능)
+const int * const p = &y; -- > 주소와 값 모두 상수라 아예 변경 불가
+
+const 의 위치가 * 앞에 붙느냐 뒤에 붙느냐에 따라 상수의 의미가 달라짐
+* 뒤에 const가 붙으면 주소가 상수인것
+
+*/
+
+
+#include <stdio.h>
+main(void)
+{
+	int x = 10, y = 20;
+	int* const CONST_POINTER = &x;
+	*CONST_POINTER = 30;
+	printf("%d\n", *CONST_POINTER);
+
+	// CONST_POINTER = &y;  << 위에서 상수로 쓸거라고 못을 박아놔서 불가함 ??
+
+	const int* p = &y;
+	// *p = 30;      값이 상수 상태라 불가함
+	p = &x;
+
+	const int* const po = &y;
+	//*po = 10;
+	//po = &x;
+
+
+	return 0;
+}
